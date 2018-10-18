ec99a93cdfb9d7fb6b5e5082d71410ef5b3c3bc2
benhard123 <benhard_tampubolon23@yahoo.com>
2018-10-18 19:56:43 +0700

    penambahan login page

--- assets/stuff/script.js
+++ assets/stuff/script.js
@@ -1,8 +1,44 @@
 function openNav(){
-	document.getElementById("sidebar").style.width="200px";
+	sidebar.style.width="200px";
 }
 
 function closeNav(){
-	document.getElementById("sidebar").style.width="0px";
-	document.getElementById("sidebar").style.width="";
+	sidebar.style.width="0px";
+	sidebar.style.width="";
+}
+
+function username(){
+	username1.style.fontSize="12px";
+	username1.style.zIndex="0";
+	if(username1.style.fontSize=="12px" && username1.style.zIndex=="0"){
+		username1.style.position="static";
+		username1.style.padding="0";
+	}
+}
+
+function checkFillUsername(){
+	if(username2.value==""){
+		username1.style.fontSize="";
+		username1.style.zIndex="";
+		username1.style.position="";
+		username1.style.padding="";
+	}
+}
+
+function password(){
+	password1.style.fontSize="12px";
+	password1.style.zIndex="0";
+	if(password1.style.fontSize=="12px" && password1.style.zIndex=="0"){
+		password1.style.position="static";
+		password1.style.padding="0";
+	}
+}
+
+function checkFillPassword(){
+	if(password2.value==""){
+		password1.style.fontSize="";
+		password1.style.zIndex="";
+		password1.style.position="";
+		password1.style.padding="";
+	}
 }
