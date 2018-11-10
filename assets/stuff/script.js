a80d2f149e0fa2e71d810573e8a76ddcb19b683a
benhard123 <benhard_tampubolon23@yahoo.com>
2018-11-10 18:43:32 +0700

    membuat javascript web menjadi lebih simpel

--- assets/stuff/script.js
+++ assets/stuff/script.js
@@ -15,84 +15,15 @@ function closeNav(){
 	content.style.marginLeft=""
 }
 
-function username(){
-	username1.style.fontSize="12px";
-	username1.style.zIndex="0";
-	username1.style.position="static";
-	username1.style.padding="0";
+function test(x){
+	x.getElementsByTagName('span')[0].style.fontSize="12px";
+	x.getElementsByTagName('span')[0].style.zIndex="0";
+	x.getElementsByTagName('span')[0].style.position="static";
+	x.getElementsByTagName('span')[0].style.padding="0";
 }
 
-function checkFillUsername(){
-	if(username2.value==""){
-		username1.style.fontSize="";
-		username1.style.zIndex="";
-		username1.style.position="";
-		username1.style.padding="";
-	}
-}
-
-function Name(){
-	name1.style.fontSize="12px";
-	name1.style.zIndex="0";
-	name1.style.position="static";
-	name1.style.padding="0";
-}
-
-function checkFillName(){
-	if(name2.value==""){
-		name1.style.fontSize="";
-		name1.style.zIndex="";
-		name1.style.position="";
-		name1.style.padding="";
-	}
-}
-
-function Email(){
-	email1.style.fontSize="12px";
-	email1.style.zIndex="0";
-	email1.style.position="static";
-	email1.style.padding="0";
-}
-
-function checkFillEmail(){
-	if(email2.value==""){
-		email1.style.fontSize="";
-		email1.style.zIndex="";
-		email1.style.position="";
-		email1.style.padding="";
-	}
-}
-
-function password(){
-	password1.style.fontSize="12px";
-	password1.style.zIndex="0";
-	password1.style.position="static";
-	password1.style.padding="0";
-	
-}
-
-function checkFillPassword(){
-	if(password2.value==""){
-		password1.style.fontSize="";
-		password1.style.zIndex="";
-		password1.style.position="";
-		password1.style.padding="";
-	}
-}
-
-function passwordC(){
-	passwordC1.style.fontSize="12px";
-	passwordC1.style.zIndex="0";
-	passwordC1.style.position="static";
-	passwordC1.style.padding="0";
-	
-}
-
-function checkFillPasswordC(){
-	if(passwordC2.value==""){
-		passwordC1.style.fontSize="";
-		passwordC1.style.zIndex="";
-		passwordC1.style.position="";
-		passwordC1.style.padding="";
+function checkFill(x){
+	if(x.getElementsByTagName('input')[0].value==""){
+		x.getElementsByTagName('span')[0].style="";
 	}
 }
