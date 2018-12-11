2027333081de7f7e4f4c30f95f5bc5da50a40c1e
Benhard Tampubolon <benhard_tampubolon23@yahoo.com>
2018-12-11 23:37:09 +0700

    fixing some mistake

--- application/views/article.php
+++ application/views/article.php
@@ -2,7 +2,7 @@
 <html>
 	<head>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>Home</title>
+		<title>Artikel</title>
 		<link rel="stylesheet" href="<?php echo base_url(); ?>/assets/stuff/tampilan_home.css">
 	</head>
 	<body>
