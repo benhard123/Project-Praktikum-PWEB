ac940a7466dc02ca4fa9f2f940d83a14b7b4f92a
Benhard Tampubolon <benhard_tampubolon23@yahoo.com>
2018-12-13 06:30:18 +0700

    memperbaiki fitur edit

--- application/controllers/C_edit.php
+++ application/controllers/C_edit.php
@@ -2,11 +2,12 @@
 	class C_edit extends CI_Controller {
 
 		public function edit(){
-			$config['upload_path']          = realpath('C:\xampp\htdocs\sadaya\assets\gambar_tempat');
+			$config['upload_path']          = realpath('.\assets\gambar_tempat');
 			$config['allowed_types']        = 'gif|jpg|png';
 			$this->load->library('upload', $config);
-			$this->upload->do_upload('gambar');
-			$this->upload->data()['file_name'];
+			echo $config['upload_path'];
+			echo $this->upload->do_upload('gambar');
+			echo $this->upload->data()['file_name'];
 		}	
 	}
 ?>
