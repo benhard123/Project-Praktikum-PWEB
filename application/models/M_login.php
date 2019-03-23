a7b48862e9a0a82d1a29a6776491f98f8cfee482
Benhard Tampubolon <benhard_tampubolon23@yahoo.com>
2019-03-23 23:05:25 +0700

    security update

--- application/models/M_login.php
+++ application/models/M_login.php
@@ -19,7 +19,7 @@ class M_login extends CI_Model{
 
 	public function auth($data){
 		$password_query = md5($data['password']);
-		$query = $this->db->query("SELECT * FROM `user` WHERE `username` = '".$data['username']."' AND `password` COLLATE latin1_bin = '".$password_query."'");
+		$query = $this->db->query("SELECT * FROM `user` WHERE `username` = ".$this->db->escape($data['username'])." AND `password` COLLATE latin1_bin = '".$password_query."'");
 		$ok=$this->db->last_query();
 		return $query->result_array();
 	}
